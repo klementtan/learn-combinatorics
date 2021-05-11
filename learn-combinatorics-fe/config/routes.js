0c3f4c90e4ef460b07da310db0c14e4eab125a58
Klement Tan <klementtan@gmail.com>
2021-05-11 17:11:58 +0800

    update

--- learn-combinatorics-fe/config/routes.js
+++ learn-combinatorics-fe/config/routes.js
@@ -38,6 +38,7 @@ export default [
                 path: '/user-profile',
                 name: 'profile',
                 icon: 'user',
+                exact: true,
                 component: './Profile',
               },
               {
