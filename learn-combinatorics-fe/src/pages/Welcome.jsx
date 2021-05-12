62c6b268c8f522115ce85ff8fcea14378d46d600
Klement Tan <klementtan@gmail.com>
2021-05-13 03:46:08 +0800

    tmep

--- learn-combinatorics-fe/src/pages/Welcome.jsx
+++ learn-combinatorics-fe/src/pages/Welcome.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { PageContainer } from '@ant-design/pro-layout';
-import { Card, Alert, Typography } from 'antd';
+import { Card, Alert,Row, Form, Input,Divider, Col ,Typography } from 'antd';
 import { useIntl, FormattedMessage, connect } from 'umi';
 import styles from './Welcome.less';
+import NusEmailVerification from "@/components/Settings/NusEmailVerification";
 
 const CodePreview = ({ children }) => (
   <pre className={styles.pre}>
@@ -29,46 +30,44 @@ const Settings = (props) => {
   return (
     <PageContainer>
       <Card>
-        <Alert
-          message={intl.formatMessage({
-            id: 'pages.welcome.alertMessage',
-            defaultMessage: '更快更强的重型组件，已经发布。',
-          })}
-          type="success"
-          showIcon
-          banner
-          style={{
-            margin: -12,
-            marginBottom: 24,
-          }}
-        />
-        <Typography.Text strong>
-          <FormattedMessage id="pages.welcome.advancedComponent" defaultMessage="高级表格" />{' '}
-          <a
-            href="https://procomponents.ant.design/components/table"
-            rel="noopener noreferrer"
-            target="__blank"
+      <Row
+        style={{
+          marginTop: '1em',
+        }}
+        justify={'center'}
+      >
+        <Col>
+          <Form
+            name="basic"
+            initialValues={{
+              name: currentUser.name,
+              primary_email: currentUser.primary_email,
+              nus_email: currentUser.nus_email,
+            }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
           >
-            <FormattedMessage id="pages.welcome.link" defaultMessage="欢迎使用" />
-          </a>
-        </Typography.Text>
-        <CodePreview>yarn add @ant-design/pro-table</CodePreview>
-        <Typography.Text
-          strong
-          style={{
-            marginBottom: 12,
-          }}
-        >
-          <FormattedMessage id="pages.welcome.advancedLayout" defaultMessage="高级布局" />{' '}
-          <a
-            href="https://procomponents.ant.design/components/layout"
-            rel="noopener noreferrer"
-            target="__blank"
-          >
-            <FormattedMessage id="pages.welcome.link" defaultMessage="欢迎使用" />
-          </a>
-        </Typography.Text>
-        <CodePreview>yarn add @ant-design/pro-layout</CodePreview>
+            <Form.Item
+              label="Name"
+              name="name"
+              rules={[{ required: true, message: 'Please input your name!' }]}
+            >
+              <Input />
+            </Form.Item>
+
+            <Form.Item label="Primary Email" name="primary_email">
+              <Input disabled />
+            </Form.Item>
+            <Form.Item>
+              <Button type="primary" htmlType="submit" loading={loading}>
+                Save
+              </Button>
+            </Form.Item>
+          </Form>
+          <Divider plain>Verify NUS email</Divider>
+          <NusEmailVerification />
+        </Col>
+      </Row>
       </Card>
     </PageContainer>
   );
