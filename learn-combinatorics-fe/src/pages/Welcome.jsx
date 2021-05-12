db8c841985420e2e8b3a268a20a6602f59485aa3
Klement Tan <klementtan@gmail.com>
2021-05-13 03:39:35 +0800

    temp

--- learn-combinatorics-fe/src/pages/Welcome.jsx
+++ learn-combinatorics-fe/src/pages/Welcome.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { PageContainer } from '@ant-design/pro-layout';
 import { Card, Alert, Typography } from 'antd';
-import { useIntl, FormattedMessage } from 'umi';
+import { useIntl, FormattedMessage, connect } from 'umi';
 import styles from './Welcome.less';
 
 const CodePreview = ({ children }) => (
